e7562238dbcaed4111687a3af50c73506e68d0ac
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-11-05 18:20:57 +0100

    visualizzo le icone di colore diverso in base al tipo

--- main.js
+++ main.js
@@ -97,35 +97,44 @@ const icons = [
     }
 ];
 
-
-icons.forEach((icon) => {
-  // console.log(icon);
-
-  const {name,prefix,family} = icon
-
-  console.log(name,prefix,family);
-
-  $('.icons-container').append(`
-    <div>
-      <i class="${family} ${prefix}${name}"></i>
-      <span>${name}</span>
-    </div>
-  `);
-
-});
-
-
+// creo un arrai di colori
 const colors = ['purple', 'tomato', 'lightgreen'];
-
+// creo un array vuoto in cui inserire i tipi delle icone
 const iconTypes = [];
-
+// scorro tutte le icone
 icons.forEach((icon) => {
-
+// estraggo solo il tipo
   const {type} = icon
-
+// se il tipo non è contenuno nell'array lo pusho
   if (!iconTypes.includes(type)) {
     iconTypes.push(type)
   }
 })
 
 console.log(iconTypes);
+
+// scorro tutte le icone
+icons.forEach((icon) => {
+  // console.log(icon);
+
+  const {name,prefix,family,type} = icon
+  // estraggo nome prefix e family e type delle icone
+
+  console.log(name,prefix,family,type);
+  // stampo in pagina un div con dentro l'icona col proprio nome
+
+  // trovo l'indice del tipo dell'icona corrente
+  const typeIndex = iconTypes.indexOf(type)
+
+  console.log(name, typeIndex);
+  // associo l'indice del tipo al colore corrispondente
+  const iconColor = colors[typeIndex]
+  console.log(iconColor);
+  $('.icons-container').append(`
+    <div>
+      <i class="${family} ${prefix}${name}" style="color:${iconColor}"></i>
+      <span>${name}</span>
+    </div>
+  `);
+
+});
